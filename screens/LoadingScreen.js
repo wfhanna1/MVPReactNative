392c47ece124f365085e528e4fb602182461473d
Jen Dougher <jennifer.dougher@insight.com>
2019-11-11 14:25:32 -0500

    initial commit

---
+++ screens/LoadingScreen.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+function App() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>
+          (Place icon here)
+        </Text>
+      </View>
+    );
+}
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: '40%',
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  title: {
+    fontSize: 40,
+  }
+});
