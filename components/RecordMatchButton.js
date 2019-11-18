f9678a59b6b0ba7904212dc93f81d4886672f1ee
Jen Dougher <jennifer.dougher@insight.com>
2019-11-18 17:31:42 -0500

    install radial gradient

--- components/RecordMatchButton.js
+++ components/RecordMatchButton.js
@@ -1,20 +1,34 @@
 import React, { Component } from 'react';
 import { StyleSheet } from 'react-native';
 import { Button, Text, View } from 'native-base';
+import LinearGradient from 'react-native-linear-gradient';
 
 export default class RecordMatchButton extends Component {
   render() {
     return (
-      <View>
+      <View style={styles.boxShadow}>
+      <LinearGradient start={{x: 0, y: 0}} end={{x:1, y:0}} locations={[0,0.7]} colors={['#983794', '#4B285F']} style={styles.linearGradient}>
         <Button rounded style={styles.button}>
           <Text style={styles.text}>Record Match</Text>
         </Button>
+        </LinearGradient>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create ({
+  boxShadow: {
+    shadowColor: '#rgba(0,0,0,0.3)',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.8,
+    shadowRadius: 1,
+    elevation: 5
+  },
+  linearGradient: {
+    borderRadius: 50,
+   
+  },
   text: {
     fontSize: 25,
     fontFamily: 'KlinicSlab-Book',
@@ -25,8 +39,9 @@ const styles = StyleSheet.create ({
     textAlign: 'center'
   },
   button: {
-    backgroundColor: '#4B285F',
+    backgroundColor: 'transparent',
     width: 195,
     height: 54,
+    
   }
 });
