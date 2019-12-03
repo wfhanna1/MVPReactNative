059c6c21d16d8c4f94a2119569e69fe9a849a8a6
Jen Dougher <jennifer.dougher@insight.com>
2019-12-03 15:48:10 -0500

    update echo line

--- ios/appcenter-post-clone.sh
+++ ios/appcenter-post-clone.sh
@@ -3,7 +3,7 @@
 # Force the 1.7.5 version of cocoapods, as opposed to the version installed by AppCenter
 echo "Uninstalling all cocoapods versions"
 sudo gem uninstall cocoapods --all
-echo "Installing cocoapods version 1.7.5"
+echo "Installing cocoapods version 1.8.4"
 sudo gem install cocoapods -v 1.8.4
 
 # Upgrade Node to a version expected by React Native 0.60
