dee355645e90169593acafdff5aed7865fd1a6b7
Jen Dougher <jennifer.dougher@insight.com>
2019-12-04 16:40:29 -0500

    #66579-mock-match-recorded-screen-and-associated-player-component

--- screens/MatchRecordedScreen.js
+++ screens/MatchRecordedScreen.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { StyleSheet, ScrollView } from 'react-native';
 import { withNavigation } from 'react-navigation';
+import ColorHeading from '../components/ColorHeading';
+import GrayHeading from '../components/GrayHeading';
+import PlayerMatchRecorded from '../components/PlayerMatchRecorded';
+
 import HeaderSm from '../components/HeaderSmall';
 import BgImage from '../components/backgroundImage';
 
@@ -9,6 +13,15 @@ function RecordMatchScreen() {
     <BgImage>
       <ScrollView>
         <HeaderSm style={styles.title} headerTitle="Match Recorded!" />
+        <ColorHeading title="Foosball Winners" />
+        <PlayerMatchRecorded name="Player Name" totalPoints="2,439" gamePoints="+200" />
+        <PlayerMatchRecorded name="Player Name" totalPoints="2,439" gamePoints="+200" />
+        <PlayerMatchRecorded name="Player Name" totalPoints="2,439" gamePoints="+200" />
+        <GrayHeading title="Foosball Losers" />
+
+        <PlayerMatchRecorded name="Player Name" totalPoints="2,439" gamePoints="-200" />
+        <PlayerMatchRecorded name="Player Name" totalPoints="2,439" gamePoints="-200" />
+        <PlayerMatchRecorded name="Player Name" totalPoints="2,439" gamePoints="-200" />
       </ScrollView>
     </BgImage>
   );
