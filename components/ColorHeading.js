5127f5cb6c39abc7b4f65f3b053f6c620e657ab2
Jen Dougher <jennifer.dougher@insight.com>
2019-12-06 16:27:27 -0500

    fix unintentional code change in ColorHeading

--- components/ColorHeading.js
+++ components/ColorHeading.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text } from 'react-native';
-//import { Text } from 'native-base';
+import { StyleSheet } from 'react-native';
+import { Text } from 'native-base';
 
 export default function ColorHeading({ title }) {
   return (
