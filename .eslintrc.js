ec251931ffacd42e6d45c8dd20345c8e529ed949
Paul Woidke <paulwoidke@gmail.com>
2019-12-12 11:23:24 -0500

    Update ESLint config to enforce: * AirBnB style guide * "use strict" at start of files * consistent double-quotes * space before function params * newlines around object properties

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,26 @@
 module.exports = {
-  'extends': 'airbnb',
-  'parser': 'babel-eslint',
-  'env': {
-    'jest': true,
-  },
-  'rules': {
-    'no-use-before-define': 'off',
-    'react/jsx-filename-extension': 'off',
-    'react/prop-types': 'off',
-    'comma-dangle': 'off',
-    'no-return-assign': 'off'
-  },
-  'globals': {
-    "fetch": false
-  }
+	extends: "airbnb",
+	parser: "babel-eslint",
+	env: {
+		jest: true,
+	},
+	rules: {
+		"no-use-before-define": "off",
+		"react/jsx-filename-extension": "off",
+		"react/prop-types": "off",
+		strict: ["error", "global"],
+		quotes: ["error", "double"],
+		"space-before-function-paren": ["error", "always"],
+		"object-curly-newline": ["error", {
+			"ObjectExpression": "always",
+			"ObjectPattern": { "multiline": true },
+			"ImportDeclaration": "never"
+		}]
+	},
+	globals: {
+	fetch: false
+	},
+	env: {
+		es6: true
+	}
 }
