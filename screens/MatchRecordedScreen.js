94e1913f59e748c090baad23d3771f3905f6b14d
Wasim Hanna <wfhanna1@gmail.com>
2019-12-20 22:22:36 -0500

    Minor update to turn + to - for losing team

--- screens/MatchRecordedScreen.js
+++ screens/MatchRecordedScreen.js
@@ -52,7 +52,7 @@ function RecordMatchScreen ({ navigation }) {
 				<ColorHeading title={`${game.name} Winners`} />
 				{winData.map((player, index) => <PlayerMatchRecorded name={`${player.fullName}`} totalPoints={Math.floor(playerRatings.filter((item) => item.playerId === winData[index].id)[0].score)} gamePoints={`+${game.kFactor}`} />)}
 				<GrayHeading title={`${game.name} Losers`} />
-				{loseData.map((player, index) => <PlayerMatchRecorded name={`${player.fullName}`} totalPoints={Math.floor(playerRatings.filter((item) => item.playerId === winData[index].id)[0].score)} gamePoints={`+${game.kFactor}`} />)}
+				{loseData.map((player, index) => <PlayerMatchRecorded name={`${player.fullName}`} totalPoints={Math.floor(playerRatings.filter((item) => item.playerId === winData[index].id)[0].score)} gamePoints={`-${game.kFactor}`} />)}
 				<ButtonPrimary
 					title="Back to Home"
 					onPress={() => navigation.popToTop("Players")}
