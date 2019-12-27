b2413d9d637743f49d2cfb0c83b42535898a7393
Paul Woidke <paulwoidke@gmail.com>
2019-12-27 14:37:56 -0500

    Refactor Match Recorded screen to use correct API calls

--- screens/MatchRecordedScreen.js
+++ screens/MatchRecordedScreen.js
@@ -12,21 +12,15 @@ import GrayHeading from "../components/GrayHeading";
 import PlayerMatchRecorded from "../components/PlayerMatchRecorded";
 import HeaderSm from "../components/HeaderSmall";
 import BgImage from "../components/backgroundImage";
-import playerRating from "../queries/playerRating";
 import ButtonPrimary from "../components/ButtonPrimary";
 
 function RecordMatchScreen ({ navigation }) {
 	const navigationContext = navigation.state.params || {
 	};
-	const [recordMatch, recordMatchLoading, recordMatchError] = useQuery(recordMatchQuery(navigationContext.recordMatch));
+	const [recordMatch, recordMatchLoading] = useQuery(recordMatchQuery(navigationContext.recordMatch));
 	const [game, gameLoading] = useQuery(gamesQuery(navigation.getParam("recordMatch").gameId));
-	// const [player, playerLoading] = useQuery(findPlayersQuery(navigation.getParam("recordMatch").players[0].playerId));
-	const [players, playersLoading] = useQuery(findPlayersQuery());
-	// const [player, setPlayers] = useState([]);
-	// const [playerRating, playerRatingLoading] = useQuery(playerRatingQuery(navigation.getParam("recordMatch").players[0].playerId));
-	const [playerRatings, playerRatingsLoading] = useQuery(playerRatingQuery());
 
-	if (!game || gameLoading || !players || playersLoading || !playerRatings || playerRatingsLoading) {
+	if (!recordMatch || recordMatchLoading || !game || gameLoading) {
 		return (
 			<BlankScreen />
 		);
@@ -34,25 +28,15 @@ function RecordMatchScreen ({ navigation }) {
 
 	const winners = navigation.getParam("recordMatch").players.filter((item) => item.isWinner);
 	const losers = navigation.getParam("recordMatch").players.filter((item) => !item.isWinner);
-	const winData = [];
-	const loseData = [];
-	winners.forEach((item) => {
-		const addPlayer = players.filter((match) => match.id === item.playerId)[0];
-		winData.push(addPlayer);
-	});
-	losers.forEach((item) => {
-		const addPlayer = players.filter((match) => match.id === item.playerId)[0];
-		loseData.push(addPlayer);
-	});
 
 	return (
 		<BgImage>
 			<ScrollView>
 				<HeaderSm style={styles.title} headerTitle="Match Recorded!" />
 				<ColorHeading title={`${game.name} Winners`} />
-				{winData.map((player, index) => <PlayerMatchRecorded name={`${player.fullName}`} totalPoints={Math.floor(playerRatings.filter((item) => item.playerId === winData[index].id)[0].score)} gamePoints={`+${game.kFactor}`} />)}
+				{winners.map((player) => <PlayerMatchRecorded id={player.playerId} gamePoints={`+${game.kFactor}`} />)}
 				<GrayHeading title={`${game.name} Losers`} />
-				{loseData.map((player, index) => <PlayerMatchRecorded name={`${player.fullName}`} totalPoints={Math.floor(playerRatings.filter((item) => item.playerId === winData[index].id)[0].score)} gamePoints={`-${game.kFactor}`} />)}
+				{losers.map((player) => <PlayerMatchRecorded id={player.playerId} gamePoints={`-${game.kFactor}`} />)}
 				<ButtonPrimary
 					title="Back to Home"
 					onPress={() => navigation.popToTop("Players")}
