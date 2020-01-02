9331d84155933e99deecd914fcc01225b1aaa538
Paul Woidke <paulwoidke@gmail.com>
2020-01-02 12:19:26 -0500

    66651 Add pull-to-refresh to Top Players and Recent Games screens

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { StyleSheet, ScrollView, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, ScrollView, View, RefreshControl } from "react-native";
 import useQuery from "../hooks/useQuery";
 import topPlayersQuery from "../queries/topPlayers";
 
@@ -11,40 +11,63 @@ import GrayHeading from "../components/GrayHeading";
 import Player from "../components/Player";
 import BgImage from "../components/backgroundImage";
 
+import insightMVPUrl from "../queries/config";
+
+function updateTopPlayers () {
+	return fetch(`${insightMVPUrl}/Ratings/topPlayers`).then((response) => response.json());
+}
+
 function HomeScreen () {
 	const [topPlayers, topPlayersLoading] = useQuery(topPlayersQuery());
+	const [topPlayersData, setTopPlayersData] = useState(false);
+	const [refreshing, setRefreshing] = useState(false);
 
-	if (!topPlayers || topPlayersLoading) {
+	const onRefresh = React.useCallback(() => {
+		setRefreshing(true);
+
+		updateTopPlayers().then((data) => {
+			setTopPlayersData(data);
+			setRefreshing(false);
+		});
+	}, [refreshing]);
+
+	if ((!topPlayers || topPlayersLoading)) {
 		return (
 			<LoadingScreen />
 		);
 	}
 
 	return (
-		<ScrollView>
-			<BgImage>
-				<HeaderLg />
-				<View style={styles.buttonContainer}>
-				  <AddNewPlayerButton />
-				</View>
-				<ColorHeading title="Top Player" />
-				<Player
-					key={topPlayers[0].id}
-					rank={1}
-					name={topPlayers[0].player[0].fullName}
-					points={Math.floor(topPlayers[0].average)}
-				/>
-				<GrayHeading title="Ranked Players" />
-				{topPlayers.slice(1).map((item, index) => (
+		<View>
+			<HeaderLg />
+			<ScrollView
+				refreshControl={
+					<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+				}
+			>
+				<BgImage>
+					<View style={styles.buttonContainer}>
+						<AddNewPlayerButton />
+					</View>
+					<ColorHeading title="Top Player" />
 					<Player
-						key={item.id}
-						rank={index + 2}
-						name={item.player[0].fullName}
-						points={Math.floor(item.average)}
+						key={(topPlayersData || topPlayers)[0].id}
+						rank={1}
+						name={(topPlayersData || topPlayers)[0].player[0].fullName}
+						points={Math.floor((topPlayersData || topPlayers)[0].average)}
 					/>
-				))}
-			</BgImage>
-		</ScrollView>
+					<GrayHeading title="Ranked Players" />
+					{(topPlayersData || topPlayers).slice(1).map((item, index) => (
+						<Player
+							key={item.id}
+							rank={index + 2}
+							name={item.player[0].fullName}
+							points={Math.floor(item.average)}
+						/>
+					))}
+				</BgImage>
+			</ScrollView>
+		</View>
 	);
 }
 
--- screens/RecentGamesScreen.js
+++ screens/RecentGamesScreen.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { ScrollView, View, StyleSheet, Text } from "react-native";
+import React, { useState } from "react";
+import { ScrollView, View, StyleSheet, Text, RefreshControl } from "react-native";
 import HeaderLg from "../components/HeaderLarge";
 import ColorHeading from "../components/ColorHeading";
 import AddNewPlayerButton from "../components/AddNewPlayerButton";
@@ -9,8 +9,25 @@ import useQuery from "../hooks/useQuery";
 import recentMatchesQuery from "../queries/recentMatches";
 import BlankScreen from "./BlankScreen";
 
+import insightMVPUrl from "../queries/config";
+
+function updateRecentGames () {
+	return fetch(`${insightMVPUrl}/Matches/recent`).then((response) => response.json());
+}
+
 export default function RecentGames () {
 	const [recentMatches, recentMatchesLoading] = useQuery(recentMatchesQuery());
+	const [recentMatchesData, setRecentMatchesData] = useState(false);
+	const [refreshing, setRefreshing] = useState(false);
+
+	const onRefresh = React.useCallback(() => {
+		setRefreshing(true);
+
+		updateRecentGames().then((data) => {
+			setRecentMatchesData(data);
+			setRefreshing(false);
+		});
+	}, [refreshing]);
 
 	const LosersList = (props) => {
 		const { losers } = props;
@@ -46,38 +63,44 @@ export default function RecentGames () {
 	}
 
 	return (
-		<BgImage>
-			<ScrollView>
-				<HeaderLg />
-				<View style={styles.buttonContainer}>
+		<View>
+			<HeaderLg />
+			<ScrollView
+				refreshControl={
+					<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+				}
+			>
+				<BgImage>
+					<View style={styles.buttonContainer}>
 				  <AddNewPlayerButton />
-				</View>
-				<ColorHeading title="Recent Games" style={styles.header} />
-				{recentMatches.map((recentMatch) => (
-					<View style={styles.wrapper} key={recentMatch.matchId}>
-						<View style={styles.gameDateWrapper}>
-							<Text style={styles.game}>{recentMatch.gameName}</Text>
-							<Text style={styles.date}>
-								{new Intl.DateTimeFormat("en-GB", {
-									year: "numeric",
-									month: "short",
-									day: "2-digit",
-									weekday: "short"
-								}).format(new Date(recentMatch.gameDate))}
-							</Text>
-						</View>
-						<View style={styles.container}>
-							{recentMatch.players.filter(({ isWinner }) => isWinner).map((player) => (
-								<PlayerRecentGames fullName={player.fullName} key={player.playerId} id={player.playerId} isWinner={player.isWinner} />
-							))}
-							<Versus losers={recentMatch.players.filter(({ isWinner }) => !isWinner)} winners={recentMatch.players.filter(({ isWinner }) => isWinner)} />
-							<LosersList losers={recentMatch.players.filter(({ isWinner }) => !isWinner)} />
-							<View style={styles.gameSeparator} />
-						</View>
 					</View>
-				))}
+					<ColorHeading title="Recent Games" style={styles.header} />
+					{(recentMatchesData || recentMatches).map((recentMatch) => (
+						<View style={styles.wrapper} key={recentMatch.matchId}>
+							<View style={styles.gameDateWrapper}>
+								<Text style={styles.game}>{recentMatch.gameName}</Text>
+								<Text style={styles.date}>
+									{new Intl.DateTimeFormat("en-GB", {
+										year: "numeric",
+										month: "short",
+										day: "2-digit",
+										weekday: "short"
+									}).format(new Date(recentMatch.gameDate))}
+								</Text>
+							</View>
+							<View style={styles.container}>
+								{recentMatch.players.filter(({ isWinner }) => isWinner).map((player) => (
+									<PlayerRecentGames fullName={player.fullName} key={player.playerId} id={player.playerId} isWinner={player.isWinner} />
+								))}
+								<Versus losers={recentMatch.players.filter(({ isWinner }) => !isWinner)} winners={recentMatch.players.filter(({ isWinner }) => isWinner)} />
+								<LosersList losers={recentMatch.players.filter(({ isWinner }) => !isWinner)} />
+								<View style={styles.gameSeparator} />
+							</View>
+						</View>
+					))}
+				</BgImage>
 			</ScrollView>
-		</BgImage>
+		</View>
 	);
 }
 
