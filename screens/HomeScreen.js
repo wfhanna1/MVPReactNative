1b331bc928ea1a5c7a49a5aeab92cb7707cf73d4
Paul Woidke <paulwoidke@gmail.com>
2020-01-03 15:23:03 -0500

    Handle empty array in response

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -33,6 +33,37 @@ function HomeScreen () {
 		);
 	}
 
+	console.log("topPlayers", topPlayers);
+
+	if (!topPlayers.length && !topPlayersData.length) {
+		return (
+			<View>
+				<HeaderLg />
+				<ScrollView
+					refreshControl={
+						<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+					}
+				>
+					<BgImage>
+						<View style={styles.buttonContainer}>
+							<AddNewPlayerButton />
+						</View>
+						<ColorHeading title="Top Player" />
+						<GrayHeading title="Ranked Players" />
+						{(topPlayersData || topPlayers).slice(1).map((item, index) => (
+							<Player
+								key={item.id}
+								rank={index + 2}
+								name={item.player[0].fullName}
+								points={Math.floor(item.average)}
+							/>
+						))}
+					</BgImage>
+				</ScrollView>
+			</View>
+		);
+	}
+
 	return (
 		<View>
 			<HeaderLg />
